4dfb0ae3855114e5786a53493a467e4cbd1d4679
Cue19275 <68084828+Cue19275@users.noreply.github.com>
2020-11-26 01:50:17 -0600

    3pwm FALTA EEPROM

--- Prjct3.X/navidad.asm
+++ Prjct3.X/navidad.asm
@@ -16,6 +16,14 @@
  __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
  
 ;#####VARIABLES####
+ CBLOCK
+ CUENTA ;Variable para los ciclos del timer1
+ CUENTA2
+ AUMENTO ;Variable para los ciclos del timer 1
+ ENDC
+ ;NUMERO EQU .10 ;Cuantas veces quiero que cuente el timer1
+ NUMERO2 EQU .2
+ 
 GPR UDATA
 ADC_VAL RES 1
 ADC_VAL2 RES 1
@@ -33,6 +41,12 @@ VIENE  RES 1
 YVIENE  RES 1
 BANDERAS RES 1
 MOT	RES 1
+W_TEMP	RES 1 ;Variable para el push/pop
+STATUS_TEMP RES 1 ;Variable para el push/pop
+VarT1H      RES 1 ;BYTE alto del TMR1
+VarT1L      RES 1 ;BYTE bajo del TMR1
+NUMERO	    RES 1
+ 
  
 ;####RESET VECTOR####
 RES_VECT  CODE    0x0000            ; processor reset vector
@@ -42,25 +56,7 @@ RES_VECT  CODE    0x0000            ; processor reset vector
 
 ;####TABLAS####
 ;Tabla para decodificar valores binarios hacia el display de 7 seg
-TABLA_7SEG
-    ANDLW B'00001111'
-    ADDWF PCL
-    RETLW   B'000111111' ;O
-    RETLW   B'000000110' ;1
-    RETLW   B'001011011' ;2
-    RETLW   B'001001111' ;3
-    RETLW   B'01100110' ;4
-    RETLW   B'01101101' ;5
-    RETLW   B'01111101' ;6
-    RETLW   B'00000111' ;7
-    RETLW   B'01111111' ;8
-    RETLW   B'01100111' ;9
-    RETLW   B'01110111' ;A
-    RETLW   B'01111100' ;b
-    RETLW   B'00111001' ;C
-    RETLW   B'01011110' ;d
-    RETLW   B'01111001' ;E
-    RETLW   B'01110001' ;F
+
     
     
 MAIN_PROG CODE                      ; let linker place main program
@@ -71,6 +67,7 @@ SETUP:
     CALL CONFIG_ADC
     CALL CONFIG_TXRX
     CALL CONFIG_T2CON_CCP1CON
+
     
     CLRF BAND
     CLRF BAND2
@@ -78,8 +75,8 @@ SETUP:
     CLRF YCOR
     CLRF VIENE
     CLRF BANDERAS
-    ;MOVLW .150
-    ;MOVWF CCPR2L
+    MOVLW .10
+    MOVWF NUMERO
     BSF	 BAND, 0
     
 ;####EMPIEZA MAINLOOP####     
@@ -95,14 +92,26 @@ LOOP:
     CALL    LEDS
 
     CALL    MOTORDC
+    BTFSC   PORTD, 7
+    GOTO    TEMPO_1
+    BTFSC   PORTD, 3
+    GOTO    TEMPO_2
+    BTFSC   PORTD, 6
+    GOTO    TEMPO_3
+    
+SIGUE:
+    
+    BTFSS   PORTD, 2
+    GOTO    PWM_L
+    GOTO    PWM_H
     
 
-    MOVFW   ADC_VAL
-    SUBLW   .1
-    MOVWF   MOT
-    BTFSC   STATUS, C
-    GOTO    SIGN
-    GOTO    NO_SIGN
+;    MOVFW   ADC_VAL
+;    SUBLW   .1
+;    MOVWF   MOT
+;    BTFSC   STATUS, C
+;    GOTO    SIGN
+;    GOTO    NO_SIGN
     GOTO    LOOP                         ; loop forever
    
 ;####SETUPS####
@@ -113,7 +122,10 @@ CONFIG_IO:
     BANKSEL TRISA
     CLRF    TRISA
     COMF    TRISA
-    CLRF    TRISD
+    BCF     TRISD, 2
+    BSF	    TRISD, 3
+    BSF	    TRISD, 6
+    BSF	    TRISD, 7
     CLRF    TRISB
     CLRF    TRISC
     CLRF    TRISE
@@ -342,6 +354,45 @@ SIGN:
     GOTO LOOP
 NO_SIGN:
     GOTO LOOP
-    
+   
 
+	
+PWM_H:
+;    INCF	CUENTA, F
+;    MOVLW	NUMERO2
+;    SUBWF	CUENTA,W; CUENTA - NUMERO = W
+;    BTFSC	STATUS,Z
+    GOTO	PWM_H2
+;    GOTO	LOOP
+    
+PWM_H2:
+    CLRF	CUENTA
+    BCF		PORTD, 2
+    GOTO	LOOP
+    
+PWM_L:
+    INCF	CUENTA2, F
+    MOVLW	NUMERO
+    SUBWF	CUENTA2, W; CUENTA - NUMERO = W
+    BTFSC	STATUS,Z
+    GOTO	PWM_L2
+    GOTO	LOOP
+    
+PWM_L2:
+    CLRF	CUENTA2
+    BSF		PORTD, 2
+    GOTO	LOOP
+ 
+TEMPO_1:
+    BSF	    PORTE, 2
+    GOTO    SIGUE
+    
+TEMPO_2:
+    BCF	    PORTE, 2
+    GOTO    SIGUE
+    
+TEMPO_3:
+    BCF	    PORTE, 2
+    GOTO    SIGUE    
+    
     END
